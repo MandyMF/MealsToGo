40ad1d4f78cc5f0eba2a355995d35990ef4678ab
MandyMF <mandy@fonoma.com>
2021-11-27 04:49:16 +0100

    infraestructure, icons, and spacing

--- src/features/restaurants/components/restaurant-info-card.component.tsx
+++ src/features/restaurants/components/restaurant-info-card.component.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
+import { StyleSheet, View, Image } from 'react-native';
+import { Card, Text } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import {Spacer} from '../../../components/spacer/spacer.component';
 
-const Title = styled.Text`
-  padding: 16px;
-  color: red;
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+
+const RestaurantCard = styled(Card)`
+    background-color : ${(props)=> props.theme.colors.bg.primary};
 `;
+const RestaurantCardCover = styled(Card.Cover)`
+    padding: ${(props)=> props.theme.space[3]};
+    background-color: ${(props)=> props.theme.colors.bg.primary};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props)=> props.theme.fonts.heading};
+  font-size: ${(props)=> props.theme.fontSizes.body};
+  color: ${(props)=> props.theme.colors.ui.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props)=> props.theme.fonts.body};
+  font-size: ${(props)=> props.theme.fontSizes.caption};
+`;
+
+const Info = styled.View`
+  padding: ${(props)=> props.theme.space[3]}; 
+`
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props)=> props.theme.space[2]};
+  padding-bottom: ${(props)=> props.theme.space[2]};
+`
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+/*const Open = styled(SvgXml)`
+  flex-direction: row;
+`;*/
 
 type RestaurantProps = {
   name: string;
@@ -26,19 +69,38 @@ export const RestaurantInfoCard = (restaurant: RestaurantProps) => {
     address = '100 some random street',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
+  const raitingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(raitingArray);
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Title style={styles.title}>{name}</Title>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+      <Title>{name}</Title>
+      <Section>
+      <Rating>
+      {raitingArray.map(() =>(
+      <SvgXml xml={star} width={20} height={20}></SvgXml>
+      ))}
+      </Rating>
+      <SectionEnd>
+        {isClosedTemporarily && (
+          <Text variant="label" style={{color:"red"}}>
+              CLOSED TEMPORARITY
+          </Text>
+        )}
+        <Spacer position='left' size='large'>
+          {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+        </Spacer>
+        <Spacer position='left' size='large'>
+          <Image style={{width:15, height: 15}} source={{uri:icon}}/>
+        </Spacer>
+      </SectionEnd>
+      </Section>
+      <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: 'white' },
-  cover: { padding: 20, backgroundColor: 'white' },
-  title: { padding: 16 },
-});
